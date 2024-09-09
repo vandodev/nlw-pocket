c8d54d532c2a75de231488d04ef6b704f5de7d5e
vandodev <vandoaparecido@hotmail.com>
2024-09-09 20:00:54 -0300

    :rocket: :date: :checkered_flag: Configurando o fastify

--- api/src/http/server.ts
+++ api/src/http/server.ts
@@ -1,1 +1,11 @@
-console.log("olá mundo")
+import fastify from 'fastify'
+
+const app = fastify()
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+})
