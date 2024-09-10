6ef79bab2c6e2bc1b6fbccda028b4ab3bc98d86a
vandodev <vandoaparecido@hotmail.com>
2024-09-10 08:40:07 -0300

    :rocket: :date: :checkered_flag: Retornando metas pendentes da semana

--- api/src/http/server.ts
+++ api/src/http/server.ts
@@ -6,11 +6,18 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return { pendingGoals }
+})
 
 app.post(
   '/goals',
