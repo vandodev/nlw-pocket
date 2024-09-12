62e10d404886dab11fc86fad3624429da80f9f00
vandodev <vandoaparecido@hotmail.com>
2024-09-12 08:35:15 -0300

    :rocket: :date: :checkered_flag: Alterando botão

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import logo from "./assets/logo-in-orbit.svg"
 import letsStart from "./assets/lets-start-illustration.svg"
 import { Plus } from 'lucide-react'
+import { Button } from './components/ui/button'
 
 export function App() {
   return ( 
@@ -11,10 +12,10 @@ export function App() {
         Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
       </p>
 
-      <button type="button" className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium -tracking-tight hover:bg-violet-600">
+      <Button>
           <Plus className="size-4" />
           Cadastrar meta
-      </button>
+      </Button>
 
     </div>
   )
