456f2700779bb71dec9c54279621f7180a345593
vandodev <vandoaparecido@hotmail.com>
2024-09-12 09:17:15 -0300

    :rocket: :date: :checkered_flag: Criando modal com Dialog

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,45 @@
 import logo from "./assets/logo-in-orbit.svg"
 import letsStart from "./assets/lets-start-illustration.svg"
-import { Plus } from 'lucide-react'
+import { Plus, X } from 'lucide-react'
 import { Button } from './components/ui/button'
+import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogClose, DialogDescription } from "./components/ui/dialog"
 
 export function App() {
   return ( 
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in.orbit" />
-      <img src={letsStart} alt="in.orbit" />
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-      </p>
+    <Dialog>
+      <div className="h-screen flex flex-col items-center justify-center gap-8">
+        <img src={logo} alt="in.orbit" />
+        <img src={letsStart} alt="in.orbit" />
+        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+          Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+        </p>
 
-      <Button>
-          <Plus className="size-4" />
-          Cadastrar meta
-      </Button>
+      <DialogTrigger asChild>
+        <Button>
+            <Plus className="size-4" />
+            Cadastrar meta
+        </Button>
+      </DialogTrigger>
 
-    </div>
+      </div>
+      <DialogContent>
+        <div className="flex flex-col gap-6 h-full">
+          <div className="flex flex-col gap-3">
+            <div className="flex items-center justify-between">
+              <DialogTitle>Cadastrar meta</DialogTitle>
+              <DialogClose>
+                <X className="size-5 text-zinc-600"/>
+              </DialogClose>
+            </div>
+
+              <DialogDescription>
+                Adicione atividades que te fazem bem e que você quer continuar
+                praticando toda semana.
+              </DialogDescription>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
 
