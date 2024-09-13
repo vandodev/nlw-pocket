edf390bc320c9c5be3fa5469726b4fe1000a52be
vandodev <vandoaparecido@hotmail.com>
2024-09-13 08:21:11 -0300

    :rocket: :date: :checkered_flag: Buscando resumo de metas da api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,25 @@
+import { useEffect, useState } from "react"
 import { CreateGoal } from "./components/create-goal"
 import { Summary } from "./components/summary"
-// import { EmptyGoals } from "./components/empty-goals"
+import { EmptyGoals } from "./components/empty-goals"
 import { Dialog } from "./components/ui/dialog"
 
 export function App() {
+const [summary, setSummary] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3333/summary')
+      .then (response => {
+      return response.json()
+    })
+      .then(data => {    
+      setSummary(data.summary)
+    })
+  }, [])
+
   return ( 
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary/>
+     {summary?.summary.total > 0 ? <Summary /> : <EmptyGoals />}
       <CreateGoal />
     </Dialog>
   )
