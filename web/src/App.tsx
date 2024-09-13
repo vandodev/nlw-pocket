293133037908b0b5df842b62b20708c6462e74e4
vandodev <vandoaparecido@hotmail.com>
2024-09-13 07:45:19 -0300

    :rocket: :date: :checkered_flag: Separanco criação de metas em componente

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,9 @@
 import logo from "./assets/logo-in-orbit.svg"
 import letsStart from "./assets/lets-start-illustration.svg"
-import { Plus, X } from 'lucide-react'
+import { Plus } from 'lucide-react'
 import { Button } from './components/ui/button'
-import { Label } from './components/ui/label'
-import { Input } from './components/ui/input'
-import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogClose, DialogDescription } from "./components/ui/dialog"
-import {
-  RadioGroup,
-  RadioGroupIndicator,
-  RadioGroupItem,
-} from './components/ui/radio-group'
+import { Dialog, DialogTrigger, } from "./components/ui/dialog"
+import { CreateGoal } from "./components/create-goal"
 
 
 export function App() {
@@ -30,71 +24,9 @@ export function App() {
       </DialogTrigger>
 
       </div>
-      <DialogContent>
-        <div className="flex flex-col gap-6 h-full">
-          <div className="flex flex-col gap-3">
-            <div className="flex items-center justify-between">
-              <DialogTitle>Cadastrar meta</DialogTitle>
-              <DialogClose>
-                <X className="size-5 text-zinc-600"/>
-              </DialogClose>
-            </div>
 
-              <DialogDescription>
-                Adicione atividades que te fazem bem e que você quer continuar
-                praticando toda semana.
-              </DialogDescription>
-          </div>
-
-          <form action="" className="flex-1 flex flex-col justify-between">
-            <div className="flex flex-col gap-6">
-              <div className="flex flex-col gap-2">
-                <Label htmlFor="title">Qual a atividade?</Label>
-                <Input
-                  id="title"
-                  autoFocus
-                  placeholder="Praticar exercícios, meditar, etc..."
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <Label htmlFor="title">Quantas vezes na semana?</Label>
-                <RadioGroup>
-                  <RadioGroupItem value="1">
-                    <RadioGroupIndicator />
-                    <span className="text-zinc-300 text-sm font-medium leading-none">
-                      1x na semana
-                    </span>
-                    <span className="text-lg leading-none">🥱</span>
-                  </RadioGroupItem>
-                  <RadioGroupItem value="2">
-                    <RadioGroupIndicator />
-                    <span className="text-zinc-300 text-sm font-medium leading-none">
-                      2x na semana
-                    </span>
-                    <span className="text-lg leading-none">🙂</span>
-                  </RadioGroupItem>
-                  <RadioGroupItem value="3">
-                    <RadioGroupIndicator />
-                    <span className="text-zinc-300 text-sm font-medium leading-none">
-                      3x na semana
-                    </span>
-                    <span className="text-lg leading-none">😎</span>
-                  </RadioGroupItem>
-                </RadioGroup>
-              </div>
-            </div>
-            
-            <div className="flex items-center gap-3">
-              <DialogClose asChild>
-                <Button type="button" className="flex-1" variant="secondary">
-                  Fechar
-                </Button>
-              </DialogClose>
-              <Button className="flex-1">Salvar</Button>
-            </div>
-          </form>
-        </div>
-      </DialogContent>
+      <CreateGoal />
+     
     </Dialog>
   )
 }
