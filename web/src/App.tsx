742906f0f410c1f1b55d974518aec9468a750e89
vandodev <vandoaparecido@hotmail.com>
2024-09-14 07:55:02 -0300

    :rocket: :date: :checkered_flag: Fazendo fetch com tanstack

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react"
 import { CreateGoal } from "./components/create-goal"
 import { Summary } from "./components/summary"
 import { EmptyGoals } from "./components/empty-goals"
 import { Dialog } from "./components/ui/dialog"
+import { useQuery } from '@tanstack/react-query'
 
 type SummaryResponse = {
   completed: number
@@ -18,22 +18,22 @@ type SummaryResponse = {
 }
 
 export function App() {
-const [summary, setSummary] = useState<SummaryResponse | null>(null)
 
-  useEffect(() => {
-    fetch('http://localhost:3333/summary')
-      .then (response => {
-      return response.json()
-    })
-      .then(data => {    
-      setSummary(data.summary)
-    })
-  }, [])
+const { data } = useQuery<SummaryResponse>({
+  queryKey: ['summary'],
+  queryFn: async () =>{
+    const response = await fetch('http://localhost:3333/summary')
+    const data = await response.json()
+    return data.summary
+  },
+  staleTime: 1000 * 60, // 60 seconds
+})
+
 
   return ( 
     <Dialog>
       {/* <pre>{JSON.stringify(summary, null,2)}</pre> */}
-     {summary?.total && summary.total > 0 ? <Summary /> : <EmptyGoals />}
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
       <CreateGoal />
     </Dialog>
   )
