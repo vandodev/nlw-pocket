d533df527d300a13d002048c73ae3147419afc47
vandodev <vandoaparecido@hotmail.com>
2024-09-14 08:03:50 -0300

    :rocket: :date: :checkered_flag: Separando requisição

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,32 +3,15 @@ import { Summary } from "./components/summary"
 import { EmptyGoals } from "./components/empty-goals"
 import { Dialog } from "./components/ui/dialog"
 import { useQuery } from '@tanstack/react-query'
-
-type SummaryResponse = {
-  completed: number
-  total: number
-  goalsPerDay: Record<
-    string,
-    {
-      id: string
-      title: string
-      completedAt: string
-    }[]
-  >
-}
+import { getSummary } from './http/get-summary'
 
 export function App() {
 
-const { data } = useQuery<SummaryResponse>({
-  queryKey: ['summary'],
-  queryFn: async () =>{
-    const response = await fetch('http://localhost:3333/summary')
-    const data = await response.json()
-    return data.summary
-  },
-  staleTime: 1000 * 60, // 60 seconds
-})
-
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 60 seconds
+  })
 
   return ( 
     <Dialog>
